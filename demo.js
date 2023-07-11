ed1a6c5c172325ce6f8e82e66839ea1fb5d9f69e
vaidik2626 <99650583+vaidik2626@users.noreply.github.com>
2023-07-11 15:41:53 +0530

    second

--- demo.js
+++ demo.js
@@ -1,10 +1,36 @@
 const http = require('http');
-const data= "Welcom To The CHARUSAT";
-const httpserver = http.createServer(function(req, res){
-      res.writeHead(200,"Content-Type: text/plain");
-    res.end("Welcome To The CHARUSAT");   
 
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/sum') {
+    let body = '';
+
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const { num1, num2 } = JSON.parse(body);
+
+      // Check if the inputs are valid numbers
+      if (isNaN(num1) || isNaN(num2)) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ error: 'Invalid input. Please provide valid numbers.' }));
+      } else {
+        const sum = parseFloat(num1) + parseFloat(num2);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ sum }));
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
-httpserver.listen(3000,()=>{
-    console.log("This site is listening on port 3000");
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
